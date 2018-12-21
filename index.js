9cd2feeb82425f63d53afa212232d74a2f543e9f
UMWIZERWA <agamithe8@gmail.com>
2018-12-21 16:03:03 +0200

    add output

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-function mytest()
-{
-   var q1=document.forms["myform"]["question1"].value;
-   var q2=document.forms["myform"]["question2"].value;
-   var q3=document.forms["myform"]["question3"].value;
-   var q4=document.forms["myform"]["question4"].value;
-   var q5=document.forms["myform"]["question5"].value;
-   var marks=0;
-   if(q1=="script")
-    {
-     marks=marks+2;
-  }
+$(document).ready(function (){
+    $('#blanks form').submit(function(event){
+        var q1 = $("input:radio[name=question1]:checked").val();
+        var q2 = $("input:radio[name=question2]:checked").val();
+        var q3 = $("input:radio[name=question3]:checked").val();
+        var q4 = $("input:radio[name=question4]:checked").val();
+        var q5 = $("input:radio[name=question5]:checked").val();
+        var marks=0;
+        if(q1=="script")
+        {
+           marks=marks+5;
+           }
+        if(q2=="document.getElementById("demo").innerHTML = "Hello World!";")
+        {
+           marks=marks+5;
+           }
+        if(q3=="The <"body"> section")
+        {
+           marks=marks+5;
+        }
+        if(q4=="False")
+        {
+           marks=marks+5;
+        }
+        if(q5=="alert("Hello World");")
+        {
+           marks=marks+5;
+        }
+        $(".Output").show();
+        $("#display").text("Your score is: " + marks);
+ event.preventDefault();
+ })
+ });
+ 
